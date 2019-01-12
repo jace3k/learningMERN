3f36e74fdad6e4c5cf3352a9066c225c77b45053
Jacek <jace3k@gmail.com>
2019-01-12 21:05:45 +0100

    auth system done

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://asdawas1:asdawas1@ds253284.mlab.com:53284/express-db'
+    mongoURI: 'mongodb://asdawas1:asdawas1@ds253284.mlab.com:53284/express-db',
+    secret: 'secretkey'
 };
--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const passport = require('passport');
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 const users = require('./routes/api/users');
 const profile = require('./routes/api/profile');
@@ -12,9 +17,11 @@ mongoose
     .then(() => console.log('MongoDB connected.'))
     .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
-    return res.send('Hello xD!')
-});
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require('./config/passport')(passport);
 
 //ROUTES
 app.use('/api/users', users);
